1c8bd634d1269b33f54d4113c38bea31758221fe
Alvaro Lopez <77080085+Alflopee@users.noreply.github.com>
2021-08-27 13:05:04 -0300

    Delete Promocion.java

--- src/turismo/Promocion.java
+++
@@ -1,9 +0,0 @@
-package turismo;
-
-public class Promocion extends Propuestas{
-
-	public Promocion(int costo, double tiempo, int cupo, TipoDePropuesta tipo) {
-		super(costo, tiempo, cupo, tipo);
-	}
-
-}
