ecec8d7607c079433fd6f6861b8c349bad6ebb8d
mandarinamagma <sofiabeatrizstorani@gmail.com>
2021-08-27 12:00:47 -0300

    clase usuario

---
+++ src/turismo/Usuario.java
@@ -0,0 +1,44 @@
+package turismo;
+
+public class Usuario {
+	
+	//atributos
+	protected int presupuesto;
+	protected double tiempo;
+	protected String tipoAtraccion;
+	protected String atracVisitadas;
+	
+	//constructor vacio
+	
+	
+	
+	
+	//getters and setters
+	public int getPresupuesto() {
+		return presupuesto;
+	}
+	public void setPresupuesto(int presupuesto) {
+		this.presupuesto = presupuesto;
+	}
+	public double getTiempo() {
+		return tiempo;
+	}
+	public void setTiempo(double tiempo) {
+		this.tiempo = tiempo;
+	}
+	public String getTipoAtraccion() {
+		return tipoAtraccion;
+	}
+	public void setTipoAtraccion(String tipoAtraccion) {
+		this.tipoAtraccion = tipoAtraccion;
+	}
+	public String getAtracVisitadas() {
+		return atracVisitadas;
+	}
+	public void setAtracVisitadas(String atracVisitadas) {
+		this.atracVisitadas = atracVisitadas;
+	}
+	
+
+
+}
